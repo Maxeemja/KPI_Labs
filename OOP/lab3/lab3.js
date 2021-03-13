688555527a15532bc62b9f1fb696d58d42589a7d
Max <maxeemja.inc@gmail.com>
2021-03-13 17:26:11 +0200

    lab3.js updated

--- OOP/lab3/lab3.js
+++ OOP/lab3/lab3.js
@@ -1,12 +1,9 @@
-
 //відсортувати слова за зростанням кількості голосних у них
 const str = "Ознайомлення з рядками та використання основних методів їх обробки в мові програмування Джава";
 const golosni = ["а", "е", "є", "и", "і", "ї", "о", "у", "ю", "я", "А", "Е", "Є", "И", "І", "Ї", "О", "У", "Ю", "Я"];
 
-let arr = str.replace(/[0-9]|[...]|[.,\/#!$%?\^&\*;:{}=\-_`~()@]/g, ' ').trim().split(' ');
-
-    
-let dict = {};
+let arr = str.replace(/[0-9]|[...]|[.,\/#!$%?\^&\*;:{}=\-_`~()@]/g, ' ').trim().split(' ');    
+let dict = [];
 
 for (let word of arr){
     //let w = word.toLowerCase();
@@ -17,18 +14,15 @@ for (let word of arr){
             counter++;
         }
     }
-    dict[word] = counter;
-}
-let sortable = [];
-for (let i in dict) {
-    sortable.push([i, dict[i]]);
+    dict.push([word, counter]);
 }
 
-sortable.sort(function(a, b) {
+dict.sort(function(a, b) {
     return a[1] - b[1];
 });
-for(let i in sortable){
-    sortable[i].pop();
+
+for(let i in dict){
+    dict[i].pop();
 }
 
-console.log(sortable.join(' '));
+console.log(dict.join(' '));
