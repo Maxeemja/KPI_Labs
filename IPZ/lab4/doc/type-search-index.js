84c507d66596186b9851eb05f1ec6958437fbde8
Max <maxeemja.inc@gmail.com>
2021-09-22 22:58:48 +0300

    4th labwork added

---
+++ IPZ/lab4/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.lab111.labwork4","l":"Coordinates"},{"p":"com.lab111.labwork4","l":"Image"},{"p":"com.lab111.labwork4","l":"Point"},{"p":"com.lab111.labwork4","l":"Points"},{"p":"com.lab111.labwork4","l":"PointsFactory"},{"p":"com.lab111.labwork4","l":"Test"}];updateSearchResults();
---
+++ IPZ/lab4/src/com/lab111/labwork4/Point.java
@@ -0,0 +1,8 @@
+package com.lab111.labwork4;
+/**
+ * @author Max Hrytsiuk
+ *
+ */
+public interface Point {
+    public void draw(Coordinates coord);
+}
