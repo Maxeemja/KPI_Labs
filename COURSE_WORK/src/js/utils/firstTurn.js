3b5b11aa6d72df8462d828e2e0a2d77c4456dec2
Max <maxeemja.inc@gmail.com>
2022-05-27 12:19:35 +0300

    fixes

--- COURSE_WORK/src/js/utils/firstTurn.js
+++
@@ -1,4 +0,0 @@
-export function firstTurn () {
-  const result = Math.floor(Math.random() * 2);
-  return result === 0 ? 'Хрестики' : 'Нулики';
-}
--- COURSE_WORK/src/js/utils/handleTurn.js
+++
@@ -1,7 +0,0 @@
-export const handleTurn = (target, current) => {
-	const cross = 'X';
-
-	const nougth = 'O';
-
-	target.textContent = current === 'Хрестики' ? cross : nougth;
-};
