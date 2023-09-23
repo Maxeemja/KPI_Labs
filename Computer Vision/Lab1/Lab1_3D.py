04993e0f4f3f63e5d4d8899c31bb7825af351254
Maxeemja <Maksym_Hrytsiuk@epam.com>
2023-09-23 03:02:25 +0300

    removed .idea added CompVision lab1

--- Computer Vision/Lab1/Lab1_3D.py
+++ Computer Vision/Lab1/Lab1_3D.py
@@ -76,14 +76,37 @@ class PyramidApp:
     def draw_pyramid(self):
         self.canvas.delete("all")
 
-        # Connect the vertices to form the quadrants of the pyramid
-        quadrant_edges = [(0, 1, 2), (0, 2, 3), (0, 3, 4), (0, 4, 1)]
+        # Connect the vertices to form the faces of the pyramid
+        edges = [
+            (0, 1), (0, 2), (0, 3), (0, 4),
+            (1, 2), (2, 3), (3, 4), (4, 1)
+        ]
 
-        for edges in quadrant_edges:
-            for i in range(len(edges)):
-                v1 = self.pyramid_vertices[edges[i]]
-                v2 = self.pyramid_vertices[edges[(i + 1) % len(edges)]]
-                self.canvas.create_line(v1[0] + 250, v1[1] + 250, v2[0] + 250, v2[1] + 250)
+        for edge in edges:
+            v1 = self.pyramid_vertices[edge[0]]
+            v2 = self.pyramid_vertices[edge[1]]
+            self.canvas.create_line(v1[0] + 250, v1[1] + 250, v2[0] + 250, v2[1] + 250)
+
+    def random_position_pyramid(self):
+        changed_position = random.randint(0, 100)
+
+        # Define the 2D coordinates of the vertices for a quadrant-based view
+        quadrant_view_vertices = [
+            (100, 250),  # Apex of the pyramid
+            (50, 350),  # Base vertex 1 (Quadrant I)
+            (150, 350),  # Base vertex 2 (Quadrant II)
+            (150, 450),  # Base vertex 3 (Quadrant III)
+            (50, 450),  # Base vertex 4 (Quadrant IV)
+        ]
+
+        if random.choice([True, False]):
+            quadrant_view_vertices = [(points[0] + changed_position * 2, points[1] - changed_position * 2) for points in
+                                  quadrant_view_vertices]
+        else:
+            quadrant_view_vertices = [(points[0] - changed_position / 2, points[1] - changed_position * 2) for points in
+                                  quadrant_view_vertices]
+
+        return quadrant_view_vertices
 
     def random_draw_pyramid(self):
         self.canvas.delete("all")
@@ -97,39 +120,89 @@ class PyramidApp:
             "thistle", "tomato", "turquoise", "violet", "yellow", "yellow green"
         ]
 
-        random_color = colors[random.randint(0, len(colors)-1)]
+        random_color = colors[random.randint(0, len(colors) - 1)]
 
-        # Connect the vertices to form the quadrants of the pyramid
-        quadrant_edges = [(0, 1, 2), (0, 2, 3), (0, 3, 4), (0, 4, 1)]
+        quadrant_view_vertices = self.random_position_pyramid()
 
-        for edges in quadrant_edges:
-            for i in range(len(edges)):
-                v1 = self.pyramid_vertices_origin[edges[i]]
-                v2 = self.pyramid_vertices_origin[edges[(i + 1) % len(edges)]]
-                self.canvas.create_line(v1[0] + 250, v1[1] + 250, v2[0] + 250, v2[1] + 250, fill=random_color)
+        # Connect the vertices to form the faces of the pyramid
+        edges = [
+            (0, 1), (0, 2), (0, 3), (0, 4),
+            (1, 2), (2, 3), (3, 4), (4, 1)
+        ]
 
-        self.animation_in_progress = self.root.after(1000, self.random_draw_pyramid)
-
-    def perspective_projection(self, distance):
-        perspective_vertices = []
-
-        for vertex in self.pyramid_vertices_origin:
-            x, y, z = vertex
-            perspective_x = x / (1 + z / distance)
-            perspective_y = y / (1 + z / distance)
-            perspective_vertices.append((perspective_x, perspective_y))
-
-        self.pyramid_vertices = perspective_vertices
+        for edge in edges:
+            v1 = quadrant_view_vertices[edge[0]]
+            v2 = quadrant_view_vertices[edge[1]]
+            self.canvas.create_line(v1[0], v1[1], v2[0], v2[1], fill=random_color)
 
     def update_colored_pyramid(self):
         if self.animation_in_progress:
             self.root.after_cancel(self.animation_in_progress)
             self.animation_in_progress = False
 
-        distance = 500  # Adjust this value to control the perspective effect
+        self.animate_colored_pyramid()
 
-        self.perspective_projection(distance)
-        self.random_draw_pyramid()
+    def animate_colored_pyramid(self):
+        self.canvas.delete("all")  # Remove the pyramid
+        self.root.after(500, self.random_draw_pyramid_centered)  # Wait 500 milliseconds
+        self.animation_in_progress = self.root.after(1000, self.animate_colored_pyramid)  # Re-schedule the animation
+
+    def random_draw_pyramid_centered(self):
+        self.canvas.delete("all")
+
+        colors = [
+            "blanched almond", "blue", "blue violet", "brown", "burlywood", "cadet blue",
+            "chartreuse", "chocolate", "coral", "cornflower blue", "cornsilk", "cyan",
+            "light sky blue", "light slate gray", "light steel blue", "light yellow", "lime green",
+            "pale violet red", "papaya whip", "peach puff", "peru", "pink", "plum",
+            "salmon", "sandy brown", "sea green", "seashell", "sienna", "sky blue",
+            "thistle", "tomato", "turquoise", "violet", "yellow", "yellow green"
+        ]
+
+        random_color = colors[random.randint(0, len(colors) - 1)]
+
+        # Center coordinates of the canvas
+        center_x = self.canvas.winfo_reqwidth() // 2
+        center_y = self.canvas.winfo_reqheight() // 2
+
+        quadrant_view_vertices = self.random_position_pyramid()
+
+        # Offset to center the pyramid
+        offset_x = center_x - sum(x for x, _ in quadrant_view_vertices) // len(quadrant_view_vertices)
+        offset_y = center_y - sum(y for _, y in quadrant_view_vertices) // len(quadrant_view_vertices)
+
+        # Adjust the pyramid vertices to keep it centered
+        centered_vertices = [(x + offset_x, y + offset_y) for x, y in quadrant_view_vertices]
+
+        # Define the faces of the pyramid
+        faces = [
+            [centered_vertices[0], centered_vertices[1], centered_vertices[2]],
+            [centered_vertices[0], centered_vertices[2], centered_vertices[3]],
+            [centered_vertices[0], centered_vertices[3], centered_vertices[4]],
+            [centered_vertices[0], centered_vertices[4], centered_vertices[1]],
+            [centered_vertices[1], centered_vertices[2], centered_vertices[3], centered_vertices[4]]
+        ]
+
+        # Fill the faces with the random color
+        for face in faces:
+            self.canvas.create_polygon(face, fill=random_color, outline="black")
+
+        # Define the edges of the pyramid
+        edges = [
+            (centered_vertices[0], centered_vertices[1]),
+            (centered_vertices[0], centered_vertices[2]),
+            (centered_vertices[0], centered_vertices[3]),
+            (centered_vertices[0], centered_vertices[4]),
+            (centered_vertices[1], centered_vertices[2]),
+            (centered_vertices[2], centered_vertices[3]),
+            (centered_vertices[3], centered_vertices[4]),
+            (centered_vertices[4], centered_vertices[1])
+        ]
+
+        # Draw all edges with black color
+        for edge in edges:
+            self.canvas.create_line(edge[0][0], edge[0][1], edge[1][0], edge[1][1], fill="black")
+
 
 # Create the main application window
 root = tk.Tk()
